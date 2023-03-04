974137ae8cf2556c7977baafd6cdf9c84cdc5889
James Olsen <olsenjames1116@gmail.com>
2023-03-04 13:15:28 -0800

    Fill Header component with static info

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './components/header/Header.js';
 
 function App() {
   return (
-    <div>App</div>
+    <Header />
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <h1>NFL Memory</h1>
+    );
+}
