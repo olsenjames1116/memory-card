f05d1a706c6a5ab23d368dd69ba8f5394132e068
James Olsen <olsenjames1116@gmail.com>
2023-03-05 12:45:54 -0800

    Format structure of Scoreboard

--- src/components/game/scoreboard/Scoreboard.js
+++ src/components/game/scoreboard/Scoreboard.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import CurrentScore from './currentScore/CurrentScore.js';
 import HighScore from './highScore/HighScore.js';
+import '../../../styles/game/scoreboard/Scoreboard.css';
 
 export default function Scoreboard(props) {
     const {currentScore} = props;
