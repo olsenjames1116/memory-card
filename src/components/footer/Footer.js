3a9cc58d988a336604326c5fe1346352fb624290
James Olsen <olsenjames1116@gmail.com>
2023-03-05 13:19:14 -0800

    Format structure of Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Author from './author/Author.js';
 import Sources from './sources/Sources.js';
+import '../../styles/footer/Footer.css';
 
 export default function Footer() {
     return (
         <div className='footer'>
-            Footer
             <Author />
             <Sources />
         </div>
