b39422942a40227df51e9e92ccae29d7d428b40d
James Olsen <olsenjames1116@gmail.com>
2023-03-05 13:22:00 -0800

    Format structure of Author

--- src/components/footer/author/Author.js
+++ src/components/footer/author/Author.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 export default function Author() {
     return (
-        <div className='author'>
-            Author
-        </div>
+        <div className='author'>Built and designed by <a className='author' href='https://github.com/olsenjames1116'>olsenjames1116</a></div>
     );
 }
