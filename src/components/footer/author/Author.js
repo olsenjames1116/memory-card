50ae7b05f70d2a04149bc7ad83e08fb5e37ff350
James Olsen <olsenjames1116@gmail.com>
2023-03-05 13:44:27 -0800

    Format color scheme

--- src/components/footer/author/Author.js
+++ src/components/footer/author/Author.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../../styles/footer/author/Author.css';
 
 export default function Author() {
     return (
--- src/components/footer/sources/Sources.js
+++ src/components/footer/sources/Sources.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../../../styles/footer/sources/Sources.css';
 
 export default function Sources() {
     return (
         <div className='sources'>
-            Sources
+            Images provided by <a className='sources' href='https://www.pinterest.com/pyrostache/'>@pyrostache</a> and <a className='sources' href='https://www.freepnglogos.com/'>FreePNGLogos</a>
         </div>
     );
 }
