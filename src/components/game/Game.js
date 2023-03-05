e16afdcbbad6da70a93d7a4fc563c23807efa924
James Olsen <olsenjames1116@gmail.com>
2023-03-05 12:39:53 -0800

    Format structure of Header

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import Scoreboard from './scoreboard/Scoreboard.js';
 import CardTable from './cardTable/CardTable.js';
 import NewGame from './newGame/NewGame.js';
@@ -13,27 +13,6 @@ export default function Game() {
     function resetScore() {
         setCurrentScore(0);
     }
-
-    // function startNewGame() {
-    //     const cover = document.createElement('div');
-    //     cover.classList.add('cover');
-
-    //     const popup = document.createElement('div');
-    //     popup.style.display = 'block';
-
-    //     document.querySelector('div.body').append(cover, popup);
-
-    // }
-
-    // useEffect(() => {
-
-    // }, []);
-
-    // useEffect(() => {
-    //     if(currentScore === 12) {
-    //         startNewGame();
-    //     }
-    // }, [currentScore]);
 
     return (
         <div className='game'>
